0d172fa690dee47dd877053f8466f6bf08bc9613
Joubert RedRat <eu+git@redrat.com.br>
2023-06-14 21:03:15 +0100

    finish implementation into controller and repository on infra layer

--- transaction-ms/internal/infra/controller.go
+++ transaction-ms/internal/infra/controller.go
@@ -7,12 +7,20 @@ import (
 	"joubertredrat/transaction-ms/internal/domain"
 	"net/http"
 	"reflect"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
+)
+
+const (
+	QUERY_STRING_PAGE                 = "page"
+	QUERY_STRING_ITEMS_PER_PAGE       = "items_per_page"
+	DEFAULT_PAGINATION_PAGE           = 1
+	DEFAULT_PAGINATION_ITEMS_PER_PAGE = 10
 )
 
 type ApiBaseController struct {
@@ -47,11 +55,32 @@ func NewCreditTransactionsController() CreditTransactionsController {
 
 func (c CreditTransactionsController) HandleList(usecase application.UsecaseListCreditCardTransaction) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		l, _ := usecase.Execute(application.ListCreditCardTransactionInput{
-			Page:         1,
-			ItemsPerPage: 10,
+		page, itemsPerPage := Pagination(
+			ctx.DefaultQuery(QUERY_STRING_PAGE, fmt.Sprintf("%d", DEFAULT_PAGINATION_PAGE)),
+			ctx.DefaultQuery(QUERY_STRING_ITEMS_PER_PAGE, fmt.Sprintf("%d", DEFAULT_PAGINATION_ITEMS_PER_PAGE)),
+		)
+
+		l, err := usecase.Execute(application.ListCreditCardTransactionInput{
+			Page:         page,
+			ItemsPerPage: itemsPerPage,
 		})
-		response := NewListResponse(1, 10, CreateCreditCardTransactionListResponseFromUsecase(l))
+		if err != nil {
+			switch err.(type) {
+			case domain.ErrPaginationCriteriaPage, domain.ErrPaginationCriteriaItemsPerPage:
+				ctx.JSON(http.StatusBadRequest, gin.H{
+					"error": err.Error(),
+				})
+			default:
+				t := time.Now()
+				ctx.JSON(http.StatusInternalServerError, gin.H{
+					"error": "internal server error",
+					"time":  DatetimeCanonical(&t),
+				})
+			}
+			return
+		}
+
+		response := NewListResponse(page, itemsPerPage, CreateCreditCardTransactionListResponseFromUsecase(l))
 		ctx.JSON(http.StatusOK, response)
 	}
 }
@@ -72,7 +101,7 @@ func (c CreditTransactionsController) HandleCreate(usecase application.UsecaseCr
 			return
 		}
 
-		t, _ := usecase.Execute(application.CreateCreditCardTransactionInput{
+		t, err := usecase.Execute(application.CreateCreditCardTransactionInput{
 			HolderName:   request.HolderName,
 			CardNumber:   request.CardNumber,
 			CVV:          request.CVV,
@@ -81,6 +110,15 @@ func (c CreditTransactionsController) HandleCreate(usecase application.UsecaseCr
 			Installments: request.Installments,
 			Description:  request.Description,
 		})
+		if err != nil {
+			t := time.Now()
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "internal server error",
+				"time":  DatetimeCanonical(&t),
+			})
+			return
+		}
+
 		response := CreateCreditCardTransactionResponseFromUsecase(t)
 		ctx.JSON(http.StatusCreated, response)
 	}
@@ -137,10 +175,25 @@ func (c CreditTransactionsController) HandleEdit(usecase application.UsecaseEdit
 			return
 		}
 
-		t, _ := usecase.Execute(application.EditCreditCardTransactionInput{
+		t, err := usecase.Execute(application.EditCreditCardTransactionInput{
 			TransactionID: transactionID,
 			Description:   request.Description,
 		})
+		if err != nil {
+			switch err.(type) {
+			case domain.ErrCreditCardTransactionNotFound:
+				ctx.JSON(http.StatusNotFound, gin.H{
+					"error": err.Error(),
+				})
+			default:
+				t := time.Now()
+				ctx.JSON(http.StatusInternalServerError, gin.H{
+					"error": "internal server error",
+					"time":  DatetimeCanonical(&t),
+				})
+			}
+			return
+		}
 
 		response := CreateCreditCardTransactionResponseFromUsecase(t)
 		ctx.JSON(http.StatusOK, response)
@@ -157,9 +210,24 @@ func (c CreditTransactionsController) HandleDelete(usecase application.UsecaseDe
 			return
 		}
 
-		usecase.Execute(application.DeleteCreditCardTransactionInput{
+		err := usecase.Execute(application.DeleteCreditCardTransactionInput{
 			TransactionID: transactionID,
 		})
+		if err != nil {
+			switch err.(type) {
+			case domain.ErrCreditCardTransactionNotFound:
+				ctx.JSON(http.StatusNotFound, gin.H{
+					"error": err.Error(),
+				})
+			default:
+				t := time.Now()
+				ctx.JSON(http.StatusInternalServerError, gin.H{
+					"error": "internal server error",
+					"time":  DatetimeCanonical(&t),
+				})
+			}
+			return
+		}
 
 		ctx.JSON(http.StatusNoContent, gin.H{})
 	}
@@ -200,4 +268,16 @@ func getValidatorErrors(verr validator.ValidationErrors) []RequestValidationErro
 	}
 
 	return errs
+}
+
+func Pagination(page, itemsPerPage string) (uint, uint) {
+	page = strings.TrimSpace(page)
+	itemsPerPage = strings.TrimSpace(itemsPerPage)
+
+	p64, _ := strconv.ParseUint(page, 10, 64)
+	p := uint(p64)
+	i64, _ := strconv.ParseUint(itemsPerPage, 10, 64)
+	i := uint(i64)
+
+	return p, i
 }
--- transaction-ms/internal/infra/repository.go
+++ transaction-ms/internal/infra/repository.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"joubertredrat/transaction-ms/internal/application"
 	"joubertredrat/transaction-ms/internal/domain"
-	"joubertredrat/transaction-ms/pkg"
 	"time"
 )
 
@@ -58,21 +57,64 @@ func (r CreditCardTransactionRepositoryMysql) Create(c domain.CreditCardTransact
 }
 
 func (r CreditCardTransactionRepositoryMysql) Update(c domain.CreditCardTransaction) (domain.CreditCardTransaction, error) {
-	return domain.CreditCardTransaction{
-		ID:            1,
-		TransactionID: "01H2KDJMHCTVTN0YDY10S5SNWB",
-		CardNumber:    "513073XXXXXX7489",
-		Amount: domain.Amount{
-			Value: 1250,
-		},
-		Installments:      2,
-		Description:       "usb cable",
-		TransactionStatus: []domain.TransactionStatus{},
-		CreatedAt:         pkg.TimeFromCanonical("2023-06-10 17:00:00"),
-	}, nil
+	updatedAt := time.Now()
+	c.UpdatedAt = &updatedAt
+
+	_, err := r.db.ExecContext(
+		context.Background(),
+		`UPDATE creditcard_transactions SET description = ?
+		WHERE transaction_id = ?`,
+		c.Description,
+		DatetimeCanonical(c.CreatedAt),
+	)
+	if err != nil {
+		r.logger.Error(err)
+		return domain.CreditCardTransaction{}, domain.ErrCreditCardTransactionRepositoryHouston
+	}
+
+	return c, nil
 }
 
 func (r CreditCardTransactionRepositoryMysql) DeleteByTransactionID(TransactionID string) error {
+	stmt, err := r.db.PrepareContext(
+		context.Background(),
+		`SELECT transaction_id
+		FROM creditcard_transactions
+		WHERE transaction_id = ? AND deleted_at IS NULL`,
+	)
+	if err != nil {
+		r.logger.Error(err)
+		return domain.ErrCreditCardTransactionRepositoryHouston
+	}
+	defer stmt.Close()
+
+	var found string
+	row := stmt.QueryRowContext(context.Background(), TransactionID)
+	errs := row.Scan(
+		&found,
+	)
+	if errs != nil {
+		r.logger.Error(errs)
+		switch {
+		case errs == sql.ErrNoRows:
+			return domain.NewErrCreditCardTransactionNotFound("TransactionID", TransactionID)
+		}
+		return domain.ErrCreditCardTransactionRepositoryHouston
+	}
+
+	deletedAt := time.Now()
+	_, err2 := r.db.ExecContext(
+		context.Background(),
+		`UPDATE creditcard_transactions SET deleted_at = ?
+		WHERE transaction_id = ?`,
+		DatetimeCanonical(&deletedAt),
+		TransactionID,
+	)
+	if err2 != nil {
+		r.logger.Error(err2)
+		return domain.ErrCreditCardTransactionRepositoryHouston
+	}
+
 	return nil
 }
 
@@ -98,7 +140,6 @@ func (r CreditCardTransactionRepositoryMysql) GetByTransactionID(TransactionID s
 	defer stmt.Close()
 
 	var c domain.CreditCardTransaction
-
 	row := stmt.QueryRowContext(context.Background(), TransactionID)
 	errs := row.Scan(
 		&c.ID,
@@ -123,33 +164,47 @@ func (r CreditCardTransactionRepositoryMysql) GetByTransactionID(TransactionID s
 }
 
 func (r CreditCardTransactionRepositoryMysql) GetList(p domain.PaginationCriteria) ([]domain.CreditCardTransaction, error) {
-	return []domain.CreditCardTransaction{
-		{
-			ID:            1,
-			TransactionID: "01H2KDJMHCTVTN0YDY10S5SNWB",
-			CardNumber:    "513073XXXXXX7489",
-			Amount: domain.Amount{
-				Value: 1250,
-			},
-			Installments:      2,
-			Description:       "usb cable",
-			TransactionStatus: []domain.TransactionStatus{},
-			CreatedAt:         pkg.TimeFromCanonical("2023-06-10 17:00:00"),
-		},
-		{
-			ID:            2,
-			TransactionID: "01H2V8NJQK039S5TPB2FBWW58C",
-			CardNumber:    "513073XXXXXX9915",
-			Amount: domain.Amount{
-				Value: 1725,
-			},
-			Installments:      1,
-			Description:       "good things aaa",
-			TransactionStatus: []domain.TransactionStatus{},
-			CreatedAt:         pkg.TimeFromCanonical("2023-06-10 17:00:00"),
-			UpdatedAt:         pkg.TimeFromCanonical("2023-06-11 17:16:00"),
-		},
-	}, nil
+	stmt, err := r.db.PrepareContext(
+		context.Background(),
+		`SELECT
+			id,
+			transaction_id,
+			card_number,
+			amount,
+			installments,
+			description,
+			created_at,
+			updated_at
+		FROM creditcard_transactions
+		ORDER BY creditcard_transactions.transaction_id DESC
+		LIMIT ?, ?`,
+	)
+	if err != nil {
+		r.logger.Error(err)
+		return []domain.CreditCardTransaction{}, domain.ErrCreditCardTransactionRepositoryHouston
+	}
+	defer stmt.Close()
+
+	offset, limit := OffsetLimit(p.Page, p.ItemsPerPage)
+	rows, err := stmt.QueryContext(context.Background(), offset, limit)
+	if err != nil {
+		r.logger.Error(err)
+		return []domain.CreditCardTransaction{}, domain.ErrCreditCardTransactionRepositoryHouston
+	}
+	defer rows.Close()
+
+	list := []domain.CreditCardTransaction{}
+	for rows.Next() {
+		var c domain.CreditCardTransaction
+		if err := rows.Scan(&c.ID, &c.TransactionID, &c.CardNumber, &c.Amount.Value, &c.Installments, &c.Description, &c.CreatedAt, &c.UpdatedAt); err != nil {
+			r.logger.Error(err)
+			return []domain.CreditCardTransaction{}, domain.ErrCreditCardTransactionRepositoryHouston
+		}
+
+		list = append(list, c)
+	}
+
+	return list, nil
 }
 
 type TransactionStatusRepositoryMysql struct {
@@ -221,7 +276,6 @@ func (r TransactionStatusRepositoryMysql) GetByCreditCardTransactionID(Transacti
 	defer rows.Close()
 
 	status := []domain.TransactionStatus{}
-
 	for rows.Next() {
 		var s domain.TransactionStatus
 		if err := rows.Scan(&s.ID, &s.CreditCardTransactionID, &s.Status, &s.CreatedAt); err != nil {
@@ -236,4 +290,9 @@ func (r TransactionStatusRepositoryMysql) GetByCreditCardTransactionID(Transacti
 	}
 
 	return status, nil
+}
+
+func OffsetLimit(page, itemsPerPage uint) (uint, uint) {
+	offset := (page - 1) * itemsPerPage
+	return offset, itemsPerPage
 }
