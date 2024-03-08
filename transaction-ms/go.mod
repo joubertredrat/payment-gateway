57b05fa3f388a95c12b1bb43ccbb14c50fc01eae
Joubert RedRat <eu+git@redrat.com.br>
2024-03-08 17:33:56 -0300

    update mock lib

--- transaction-ms/go.mod
+++ transaction-ms/go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.8.4
 	github.com/urfave/cli/v2 v2.25.6
+	go.uber.org/mock v0.4.0
 	google.golang.org/grpc v1.55.0
 	google.golang.org/protobuf v1.30.0
 )
--- transaction-ms/internal/application/usecase_test.go
+++ transaction-ms/internal/application/usecase_test.go
@@ -6,8 +6,8 @@ import (
 	"joubertredrat/transaction-ms/pkg"
 	"testing"
 
-	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
 )
 
 func TestUsecaseCreateCreditCardTransaction(t *testing.T) {
